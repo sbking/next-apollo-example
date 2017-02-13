27e8c3885a5ae51d39201829654eb8c4c2af88a7
Stephen King <stephen@footstepsmarketing.com>
2017-02-13 16:42:10 -0700

    Use next-apollo NPM package

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import App from '../components/App'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
 import PostList from '../components/PostList'
-import withData from '../lib/withData'
+import { withData } from 'next-apollo'
+import apolloFactory from '../lib/apolloFactory'
 
-export default withData((props) => (
+export default withData(apolloFactory, (props) => (
   <App>
     <Header pathname={props.url.pathname} />
     <Submit />
