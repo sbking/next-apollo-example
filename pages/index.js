e3f447545a83f6f15114365d88301f625324966d
Stephen King <stephen@footstepsmarketing.com>
2017-02-21 16:47:46 -0700

    Update to next-apollo v0.0.15

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,36 @@ import App from '../components/App'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
 import PostList from '../components/PostList'
+import { initClient } from '../lib/initClient'
+import { initStore } from '../lib/initStore'
 import { withData } from 'next-apollo'
-import apolloFactory from '../lib/apolloFactory'
 
-export default withData(apolloFactory, (props) => (
-  <App>
-    <Header pathname={props.url.pathname} />
-    <Submit />
-    <PostList />
-  </App>
-))
+export default withData(class extends React.Component {
+  static async getInitialProps (context) {
+    return {
+      headers: context.req ? context.req.headers : {},
+      url: {
+        query: context.query,
+        pathname: context.pathname
+      }
+    }
+  }
+
+  static getApolloClient (props) {
+    return initClient(props.headers)
+  }
+
+  static getReduxStore (client, props) {
+    return initStore(client, props.initialState)
+  }
+
+  render () {
+    return (
+      <App>
+        <Header pathname={this.props.url.pathname} />
+        <Submit />
+        <PostList />
+      </App>
+    )
+  }
+})
